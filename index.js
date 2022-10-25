fe1e2f7499b7e1f25d2e8d7d8d078fafd51c0db4
kamruzzaman22874 <joybd.eee@gmail.com>
2022-10-26 00:33:00 +0600

    Loaded data

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.use(cors());
 
 const Port = process.env.Port || 5000;
 
+
+const courses = require('./data.json');
+
+app.get('/courses', (req, res)=>{
+    res.send(courses)
+})
 app.listen(Port, () => {
-    console.log('server is running on port', Port)
+    console.log('server is running on port', Port);
 })
